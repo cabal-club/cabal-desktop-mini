8085f37274e52ea690685601530f2727c5dacdab
nikolaiwarner <n@nikolaiwarner.com>
2019-02-13 14:56:30 -0500

    Update icons

--- views/peers.js
+++ views/peers.js
@@ -15,7 +15,7 @@ module.exports = function (state, emit) {
   return html`
     <body class="sans-serif">
       <nav class="ph4 pt4">
-        <a class="f6 link dim br3 ph3 pv2 mb1 dib white bg-black" href="/messenger">⬅ Back</a>
+        <a class="f6 link br3 ph3 pv2 mb1 dib black dim ba b--black" href="/messenger">⬅</a>
       </nav>
       <h1 class="ph4 f3 f2-m f1-l">${TITLE}</h1>
 
